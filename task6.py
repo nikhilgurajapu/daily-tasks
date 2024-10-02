0de8c08fa16f96abcef2feefb00cf1a0a26db7a3
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-02 12:02:34 +0530

    gratest number program

---
+++ task6.py
@@ -0,0 +1,12 @@
+#program to print lagest among the three numbers
+
+fn = 21
+sn = 11
+tn = 2
+
+if fn>sn and fn > tn :
+    print(fn)
+if sn> fn and sn > tn:
+    print(sn)
+if tn > fn and tn > sn:
+    print(tn)

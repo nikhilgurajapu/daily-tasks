3eb888426571db3a84a0cf2c08cc5f2102bafeeb
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-03 11:40:05 +0530

    count 0f even and odd in list

---
+++ task10.py
@@ -0,0 +1,16 @@
+"""program that counts the number of even and odd numbers in a list"""
+li = [1,2,3,4,5,6,7,8,9]
+count_li = len(li)
+even_li = []
+odd_li = []
+for i in range(1 , count_li + 1):
+    if i % 2 == 0:
+        even_li.append(i)
+for i in range(1 , count_li + 1):
+    if i % 2 != 0:
+        odd_li.append(i)
+count_even = len(even_li)
+count_odd = len(odd_li)
+print(f'number of even numbers is {count_even}')
+print(f'number of odd numbers is {count_odd}')
+    

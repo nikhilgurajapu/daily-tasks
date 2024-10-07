32f87ac5d09c5276f1e2934741d71635e0f80166
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-07 19:03:08 +0530

    palindrome program

---
+++ 7-10-2024/task3.py
@@ -0,0 +1,7 @@
+"""Python program to check a String is palindrome or not. """
+given_input = "malayalam"
+is_palindrome = given_input[::-1]
+if given_input == is_palindrome :
+    print("yes its a palindrome")
+else:
+    print("No its not a palindrome")

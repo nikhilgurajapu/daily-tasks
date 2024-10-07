968f6a3de916502454e19b5e6ac47f6751eb19fa
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-07 19:36:00 +0530

    program with is_digit()

---
+++ 7-10-2024/task7.py
@@ -0,0 +1,7 @@
+""" Python program to check given character is digit or not using isdigit() method"""
+given_input = "add@123"
+for char in given_input:
+    if char.isdigit():
+        break
+print("yes given_input having digit")
+

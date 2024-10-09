8ae13f8844245a04cd540bd0ed176b2a389f240b
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-09 14:35:54 +0530

    practicicng of list methods

---
+++ 7-10-2024/lists/task1.py
@@ -0,0 +1,83 @@
+list_1 = [1,2,3,"six","eight"]
+"""
+append
+insert
+extend
+
+"""
+"""s = ["a","b","d","j","a"]
+s.append("six")
+print(s)"""
+"""ss=[]
+for i in list_1:
+    ss.append(i)
+    print(ss)
+print("---------------")
+print(ss)"""
+
+
+
+"""list_1.insert(1,"c")
+
+
+l = [1,2,3,4]
+l1 = [5,6,7,"s",0.5]
+l1.extend("six")
+
+l.extend(l1)
+print(l)"""
+
+#remove
+
+"""
+remove
+pop
+delete
+clear
+"""
+l = [1,2,3,4,5,6]
+
+"""l.pop()
+print(l)"""
+
+"""del l
+print(l)"""
+
+"""l.clear()
+print(l)"""
+
+"""p = ["k","K","A","a","r","i","m"]
+p.sort()
+print(p)"""
+
+"""name = ["n","i","khil","karim"]
+name.reverse()
+print(name)"""
+
+"""n = "string"
+r = list(n)
+print(r)"""
+
+"""
+str()
+int()
+set()
+"""
+
+nums = "1 2 3 4"
+num_list = nums.split()
+print(num_list)
+
+i = [6,5,4,3,2]
+"""print(sorted(i))"""
+i.pop()
+print(i)
+
+
+
+
+
+
+
+
+

ee8b8c1ff568912da77b24d303f527ea5ab799fa
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-11 10:52:22 +0530

    nested list task

---
+++ 10-10-2024/task14.py
@@ -0,0 +1,7 @@
+"""1.Write a Python program to create a 3x3 matrix (nested list) and print the matrix. Then, access and print the element at row 2, column 3.
+"""
+l = [[1,2,3],[4,5,6],[7,8,9]]
+
+for i in range(len(l)):
+    print(l[i])
+print(l[1][2])

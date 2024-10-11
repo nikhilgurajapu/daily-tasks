68870b31ebd92cecaf6abe80e57774a4820140b9
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-11 11:04:44 +0530

    nested list task 2

---
+++ 10-10-2024/task15.py
@@ -0,0 +1,5 @@
+"""Create a nested list representing a list of students' names and their respective grades. Write a Python program to print each student's name along with their grade.
+"""
+nested_l = [["arun",25],["noor",26],["ahmed",28],["prathima",25]]
+for l,m in nested_l:
+    print(f"name: {l}, age: {m}")

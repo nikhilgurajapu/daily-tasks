e8e31e888bac7fe893058a6cf37e3301f0d7ab70
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-16 10:57:22 +0530

    task6

--- 15-10-2024/15-10 task6.py
+++ 15-10-2024/15-10 task6.py
@@ -1,5 +1,13 @@
 #7. Merge Two Lists: Create two lists and write a program to merge them into a single list, ensuring there are no duplicates.
-l1 = [1,2,3,"four","five",0.6]
-l2 = ["nine",2,"eight","five",0.8]
-l3 = l1 + l2
-print(l3)
+l1 = ["eleven","ten","two","four","five"]
+l1_len = len(l1)
+l2 = ["nine","two","eight","five","ten"]
+l2_len = len(l2)
+for i in l1_len:
+    if l1[i] in l2:
+        l2.remove(l1[i])
+    print(l2)
+
+print(l1 + l2)
+
+

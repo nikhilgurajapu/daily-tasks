6a491297b296d0c957dfadbc19bd862aec4b7e5a
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-17 18:59:28 +0530

    sets theory

---
+++ 17-10-2024/sets methods theory.py
@@ -0,0 +1,13 @@
+"""
+we ha a set methods
+union
+diference
+intersection and
+symmetric difference
+"""
+
+#union() returns a set containing all elements of both sets.
+#diference() returns a set containing all the elements in the first set but not second.
+#intersection() returns a set containing common elements of both sets.
+#symmetric difference returns a set containing all elements which are not common to both sets.
+

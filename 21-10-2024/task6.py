803779e1eb6e877baef535522488daf31409a61b
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-21 21:17:52 +0530

    practice on function methods

---
+++ 21-10-2024/task6.py
@@ -0,0 +1,11 @@
+#program to print it is a prime or not using function
+def sum_of_squares(n):
+    m= 0
+    for i in range(1,n+1):
+        m += i
+    return sum
+n = int(input("enter the number :"))
+print(sum_of_squares(n))
+
+
+

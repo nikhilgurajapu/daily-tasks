22bac6971c245aaa74b81143da5c26fefac59106
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-10-25 15:47:07 +0530

    daily practice on package

---
+++ 26-102024.py
@@ -0,0 +1,18 @@
+#theory of packages 
+"""
+package:- it contains a group of functions and modules we can import the module to the another package and
+use
+
+ex:-
+we have a add() and mul() functions in module1.py in the package of pkg.
+then we can import
+
+from pkg.module1 import add,mul  #importing add,mul from the module1 in the package alias pkg
+"""
+
+from package1.module1 import add,mul
+x = 20
+y = 30
+
+print(add(x,y))
+print(mul(x,y))

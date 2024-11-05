f957f71abaa3d6565d9338e2cc25936ce4fea5b1
nikhil gurajapu <nikhlgurajapu123@gmail.com>
2024-11-05 13:13:50 +0530

    task on methods and nested class

---
+++ 5-11-2024.py
@@ -0,0 +1,30 @@
+#nested class:- it is nothing but a class in a class
+
+#example:-
+class employee:
+    def __init__(self,name,age,skillset):
+        self.name = name
+        self.age = age
+        self.skillset = skillset
+    
+    def display(self):
+        print(self.name)
+        print(self.age)
+        print(self.skillset)
+    
+    class employement:
+        def __init__(self,cmpnyname,salary,domain):
+            self.cmpnyname = cmpnyname
+            self.salary = salary
+            self.domain = domain
+        def display(self):
+            print(self.cmpnyname)
+            print(self.salary)
+            print(self.domain)
+    
+    m = employement("skywaves",25000,"python")
+    m.display()
+
+p = employee("nikhil",25,"html,css,js")
+p.display()
+
